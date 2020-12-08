3533d55bb986be40c59a6b9c381d467ea57392a4
Martin Kutter <kutter@chemmedia.de>
2020-12-08 15:00:35 +0100

    fix #3 by updateing dependencies

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,47 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    devtool: false,
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash:8].css',
+            chunkFilename: '[id].[contenthash:8].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                            sourceMap: false,
+                        },
+                    },
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new OptimizeCssAssetsPlugin(), "..."],
+        runtimeChunk: {
+            name: 'runtime',
+        },
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
+})
